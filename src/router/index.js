79c1f813d1ac8a4a588114fc34e8c7a870332ae4
wangtong <wangtong@co-think.com>
2020-10-10 14:53:47 +0800

    ''

--- src/router/index.js
+++ src/router/index.js
@@ -65,7 +65,7 @@ export default new Router({
             icon: 'el-icon-full-screen',
             localekey: 'fullScreen',
           }
-        },
+        }
       ]
     }
   ]
--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -1,13 +1,28 @@
 <template>
-<div>login</div>
+<div>
+  <div class="login">
+    <el-button type="primary" plain @click="submit">进入首页</el-button>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    submit() {
+      this.$router.push({
+        path: '/index'
+      })
+    }
+  }
 }
 </script>
 
 <style>
-
+.login {
+  width: 100%;
+  height: 500px;
+  text-align: center;
+  line-height: 500px;
+}
 </style>
